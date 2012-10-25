2461e4aabd4b6d4785c80bfa80a9850f4fe78c23
Alexander Garzon <venehosting@gmail.com>
2012-10-25 14:21:06 -0400

    Setting up crontab Changing version number

--- config.dev.php
+++ config.dev.php
@@ -1,6 +1,6 @@
 <?php
-define('VERSION', '1.2');
-define('HOST', 'xxx');
+define('VERSION', '1.3');
+define('HOST', 'xxx');//Example: mydomain.com
 define('THEME', 'grey');//options are: black, blue, white, yellow and grey (default)
 
 switch (THEME) {
